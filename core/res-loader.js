37c1058be3e954fe45e25f818cb1d0b5b4d211c6
ptflp <filippov.pa@s-vfu.ru>
2018-10-03 22:52:57 +0900

    complete game

--- core/res-loader.js
+++ core/res-loader.js
@@ -1,51 +1,59 @@
+
 (function() {
     var resourceCache = {};
     var loading = [];
     var readyCallbacks = [];
+
     // Load an image url or an array of image urls
     function load(urlOrArr) {
-        if (urlOrArr instanceof Array) {
+        if(urlOrArr instanceof Array) {
             urlOrArr.forEach(function(url) {
                 _load(url);
             });
-        } else {
+        }
+        else {
             _load(urlOrArr);
         }
     }
+
     function _load(url) {
-        if (resourceCache[url]) {
+        if(resourceCache[url]) {
             return resourceCache[url];
-        } else {
+        }
+        else {
             var img = new Image();
             img.onload = function() {
                 resourceCache[url] = img;
-                if (isReady()) {
-                    readyCallbacks.forEach(function(func) {
-                        func();
-                    });
+                
+                if(isReady()) {
+                    readyCallbacks.forEach(function(func) { func(); });
                 }
             };
             resourceCache[url] = false;
             img.src = url;
         }
     }
+
     function get(url) {
         return resourceCache[url];
     }
+
     function isReady() {
         var ready = true;
-        for (var k in resourceCache) {
-            if (resourceCache.hasOwnProperty(k) &&
-                !resourceCache[k]) {
+        for(var k in resourceCache) {
+            if(resourceCache.hasOwnProperty(k) &&
+               !resourceCache[k]) {
                 ready = false;
             }
         }
         return ready;
     }
+
     function onReady(func) {
         readyCallbacks.push(func);
     }
-    window.resources = {
+
+    window.resources = { 
         load: load,
         get: get,
         onReady: onReady,
