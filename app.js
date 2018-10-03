4e039cf75ac8ff0acacc72db0bf25691f66df7ce
ptflp <filippov.pa@s-vfu.ru>
2018-10-03 14:10:14 +0900

    add resource loader + resources

--- app.js
+++ app.js
@@ -4,3 +4,33 @@ var ctx = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
+
+var lastTime;
+function main() {
+    var now = Date.now();
+    var dt = (now - lastTime) / 1000.0;
+
+    update(dt);
+    render();
+
+    lastTime = now;
+    requestAnimFrame(main);
+};
+
+resources.load([
+    'img/sprites.png',
+    'img/terrain.png'
+]);
+resources.onReady(init);
+
+function init() {
+    terrainPattern = ctx.createPattern(resources.get('img/terrain.png'), 'repeat');
+
+    document.getElementById('play-again').addEventListener('click', function() {
+        reset();
+    });
+
+    reset();
+    lastTime = Date.now();
+    main();
+}
